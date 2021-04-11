31781ab1ffd90f813ca875afe8985225c93b02e2
libo <431397516@qq.com>
2021-04-11 16:18:11 +0800

    修改

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,34 @@
 import styles from './index.less';
+import React, {
+  useState,
+  useEffect,
+  useReducer,
+  useRef,
+  useImperativeHandle,
+} from 'react';
+import axios from 'axios';
 
 export default function IndexPage() {
+  const [city, setCity] = useState([]);
+  useEffect(() => {
+    axios.get('/api/tags').then(function (response) {
+      console.log(response);
+      setCity(response.data.list);
+    });
+    axios.get('/api/person').then(function (response) {
+      console.log(response);
+    });
+  }, []);
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
+      {city.map((item, index) => {
+        return (
+          <div key={index} style={{ borderBottom: '1px solid #f5f5f5' }}>
+            {item.name}
+          </div>
+        );
+      })}
     </div>
   );
 }
